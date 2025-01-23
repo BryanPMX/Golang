2e1516a99694ad16e3209510bc65a0e0ae073b03
BryanPMX <perez.bryan24@outlook.com>
2025-01-23 01:18:59 -0700

    File re-strcuture

--- WebServer/WebServer.go
+++ WebServer/WebServer.go
@@ -1,24 +1,28 @@
 /*
    This program is a simple yet complete example of a web server in Go.
-   It is typical to use Go for such applications due to its efficient handling
+   It is typical to use Go for such applications due to its efficient handling.
    of concurrency and its straightforward, powerful standard library for networking tasks.
 */
 
 // Package declaration: main is the default package for creating executable Go applications.
 package main
 
-// Importing necessary packages:
-// - fmt: Provides formatted I/O with functions analogous to C's printf and scanf.
-// - net/http: Provides HTTP client and server implementations.
+/*
+	Importing necessary packages:
+	- fmt: Provides formatted I/O with functions analogous to C's printf and scanf.
+	- net/http: Provides HTTP client and server implementations.
+*/
 import (
 	"fmt"
 	"net/http"
 )
 
-// helloWorld is a handler function that writes "Hello, World!" to the HTTP response.
-// It matches the signature required by http.HandlerFunc.
-// - w http.ResponseWriter: An interface used to send HTTP responses.
-// - r *http.Request: A data structure that represents the client HTTP request.
+/*
+helloWorld is a handler function that writes "Hello, World!" to the HTTP response.
+It matches the signature required by http.HandlerFunc.
+- w http.ResponseWriter: An interface used to send HTTP responses.
+- r *http.Request: A data structure that represents the client HTTP request.
+*/
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	// Fprintf is a function from the fmt package that formats and writes to an io.Writer object.
 	// Here, it's writing "Hello, World!" to the HTTP response writer (w).
